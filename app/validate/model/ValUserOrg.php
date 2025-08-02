8e3a6c168bfe1ff3c6797401776bc62c9cfeb114
k-shino <k-shino@nisp.co.jp>
2025-08-02 11:37:04 +0900

    ValUserOrgを追加

---
+++ app/validate/model/ValUserOrg.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace nnk2\app\validate\model;
+
+require_once __DIR__ . '/autoload.php';
+
+use nnk2\base\util\Results;
+use nnk2\base\validate\model\ValModel;
+use nnk2\app\data\model\UserOrg;
+use nnk2\app\validate\field\ValJpName;
+use nnk2\app\validate\field\ValEmail;
+
+/**
+ * ユーザ組織モデル単位のバリデーションのクラス  
+ */
+class ValUserOrg extends ValModel {
+    /**
+     * バリデーションのインスタンスを取得する
+     * @param UserOrg|array $user 対象のユーザ
+     * @param ?array $row 対象の配列
+     * @param Results $results エラーを保持
+     * @return ValUserOrg バリデーションのインスタンス
+     */
+    public static function start(UserOrg $user, ?array $row, Results $results): ValUserOrg {
+        $ValUserOrg = self::startBase(self::$self, $user, $row, $results);
+        return $ValUserOrg;
+    }
+    private static ?ValUserOrg $self = null;
+
+    /**
+     * コンストラクタ
+     */
+    private function __construct() {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function create(): ValUserOrg {
+        $model = UserOrg::cast($this->model);
+        $res = $this->results;
+        ValJpName::start($model, null, UserOrg::name,    $res)->check(true)->end();
+        ValEmail::start($model,  null, UserOrg::email,   $res)->check(true)->end();
+        ValJpName::start($model, null, UserOrg::address, $res)->check(true)->end();
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function select(): ValUserOrg {
+        $model = UserOrg::cast($this->model);
+        $row = $this->row;
+        $res = $this->results;
+        ValJpName::start($model, $row, UserOrg::name,    $res)->check(true)->end();
+        ValEmail::start($model,  $row, UserOrg::email,   $res)->check(true)->end();
+        ValJpName::start($model, $row, UserOrg::address, $res)->check(true)->end();
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function update(): ValUserOrg {
+        $model = UserOrg::cast($this->model);
+        $res = $this->results;
+        ValJpName::start($model, null, UserOrg::name,    $res)->check(true)->end();
+        ValEmail::start($model,  null, UserOrg::email,   $res)->check(true)->end();
+        ValJpName::start($model, null, UserOrg::address, $res)->check(true)->end();
+        return $this;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function end(): Results {
+        return $this->results;
+    }
+}
